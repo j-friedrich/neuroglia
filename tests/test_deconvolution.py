3387a2aa25e82ec78d00fd2d137b4a3628417dae
Johannes Friedrich <jfriedrich@flatironinstitute.org>
2018-04-25 12:30:40 -0400

    more deconvolution tests

--- tests/test_deconvolution.py
+++ tests/test_deconvolution.py
@@ -1,7 +1,7 @@
 import numpy.testing as npt
 import numpy as np
 from neuroglia.calcium.oasis.oasis_methods import oasisAR1, constrained_oasisAR1, oasisAR2, constrained_oasisAR2
-from neuroglia.calcium.oasis.functions import onnls, constrained_onnlsAR2
+from neuroglia.calcium.oasis.functions import onnls, constrained_onnlsAR2, deconvolve, estimate_parameters
 from neuroglia.datasets.synthetic_calcium import gen_data
 
 
@@ -41,3 +41,33 @@ def test_AR2():
 
 def test_constrainedAR2():
     AR2(True)
+
+
+def test_deconvolve():
+    g = [1.7, -.712]
+    sn = .2
+    y, c, s = [a[0] for a in gen_data(g, sn, N=1, seed=3)]
+    for b in [0, None]:
+        for optimize_g in [0, 10]:
+            result = deconvolve(y, g, sn, b=b, optimize_g=optimize_g)
+            npt.assert_allclose(np.corrcoef(result[0], c)[0, 1], 1, .03)
+            npt.assert_allclose(np.corrcoef(result[1], s)[0, 1], 1, .2)
+
+
+def test_onnls():
+    g, sn = .95, .3
+    y, c, s = [a[0] for a in gen_data([g], sn, N=1, seed=0)]
+    for (g_, lam) in [([g], 0), ([g], 2.4), (g**np.arange(1000), 0)]:
+        result = onnls(y, g_, lam=lam)
+        npt.assert_allclose(np.corrcoef(result[0], c)[0, 1], 1, .03)
+        npt.assert_allclose(np.corrcoef(result[1], s)[0, 1], 1, .2)
+
+
+def test_estimate_parameters():
+    sn = .3
+    for g in [[.95], [1.7, -.712]]:
+        y = gen_data(g, sn, N=1, seed=0)[0][0]
+        for nonlinear_fit in [True, False]:
+            g_, sn_ = estimate_parameters(y, len(g), nonlinear_fit=nonlinear_fit)
+            npt.assert_allclose(g_, g, .02)
+            npt.assert_allclose(sn_, sn, .02)
